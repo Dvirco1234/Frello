2767c41924824afe6332018f571d785e94fc918a
Dvir Cohen <dvirco123@gmail.com>
2022-07-19 18:43:53 +0300

    start

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,21 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import homePage from '../views/home-page.vue'
+import boardApp from '../views/board-app.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: homePage
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/board',
+      name: 'board-app',
+      component: boardApp 
     }
   ]
 })
---
+++ frontend/src/views/home-page.vue
@@ -0,0 +1,19 @@
+<template>
+    <section>
+        <h1>Hello</h1>
+    </section>
+</template>
+<script>
+export default {
+    name: '',
+    props: { type: Object },
+    data() {
+        return {}
+    },
+    created() {},
+    methods: {},
+    computed: {},
+    unmounted() {},
+    components: {},
+}
+</script>
