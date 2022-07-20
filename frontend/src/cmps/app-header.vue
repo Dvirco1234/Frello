6e4647a97e25c92a714c0a93756645e9d3a7ce2e
Dvir Cohen <dvirco123@gmail.com>
2022-07-20 17:54:58 +0300

    header nav bar

--- frontend/src/cmps/app-header.vue
+++ frontend/src/cmps/app-header.vue
@@ -1,55 +1,90 @@
 <template>
-  <section class="app-header full">
-    <section class="nav-bar flex">
-      <section class="left-side flex">
-
-        <h1 class="logo">Frello</h1>
-        <ul class="nav-links flex clean-list">
-          <li class="menu" @click="modal">Workspaces<span><img :src="arrowImg"></span>
-          </li>
-          <div class="modal" :class="{ hidden: isHidden }">ABC</div>
-          <li>Recent<span><img :src="arrowImg"></span></li>
-          <li>Starred<span><img :src="arrowImg"></span></li>
-          <li>Templates<span><img :src="arrowImg"></span></li>
-          <li class="create-btn">Create</li>
-        </ul>
-      </section>
-      <section class="right-side flex align-center">
-        <span class="info-btn"><img :src="infoImg"></span>
-      </section>
+    <section class="app-header full">
+        <section class="nav-bar flex">
+            <section class="left-side flex">
+                <h1 class="logo">Frello</h1>
+                <ul class="nav-links flex clean-list">
+                    <!-- <li class="menu" @click="modal">
+                        Workspaces<span><img :src="arrowImg" /></span>
+                    </li> -->
+                    <li v-for="btn in navBtns" :key="btn.name" :class="{opened :btn.isOpen}">
+                        <div class="nav-btn" @click="toggleDropdown(btn.name)">
+                            <span>{{btn.name}}</span>
+                            <span><img :src="arrowImg" /></span>
+                        </div>
+                        <nav-dropdown v-if="btn.isOpen" v-click-outside="toggleDropdown" :btn="btn" />
+                    </li>
+                    <li class="create-btn">Create</li>
+                </ul>
+            </section>
+            <section class="right-side flex align-center">
+                <span class="info-btn"><img :src="infoImg" /></span>
+            </section>
+        </section>
     </section>
-  </section>
 </template>
 <script>
 import router from '../router/index.js'
 import { clickOutsideDirective } from '../directives/index.js'
+import navDropdown from './nav-dropdown.vue'
 export default {
-  name: 'app-header',
-  data() {
-    return {
-      arrowImg: 'https://www.svgrepo.com/show/145159/down-arrow.svg',
-      infoImg: 'https://www.svgrepo.com/show/26894/info.svg',
-      isHidden: true
-    }
-  },
-  methods: {
-    modal() {
-      this.isHidden = !this.isHidden
-    }
-  },
+    name: 'app-header',
+    data() {
+        return {
+            arrowImg: 'https://www.svgrepo.com/show/145159/down-arrow.svg',
+            infoImg: 'https://www.svgrepo.com/show/26894/info.svg',
+            currOpenedDropdown: null,
+            navBtns: [
+                {
+                    name: 'Boards',
+                    isOpen: false,
+                    data: ''
+                },
+                {
+                    name: 'Recent',
+                    isOpen: false,
+                    data: ''
+                },
+                {
+                    name: 'Starred',
+                    isOpen: false,
+                    data: ''
+                },
+                {
+                    name: 'Templates',
+                    isOpen: false,
+                    data: ''
+                },
+            ],
+        }
+    },
+    methods: {
+        toggleDropdown(name = '') {
+            // this.isDropdownOpen = !this.isDropdownOpen
+            this.navBtns.map(btn => btn.isOpen = false)
+            if(!name) return
+            const idx = this.navBtns.findIndex(btn => btn.name === name)
+            this.navBtns[idx].isOpen = true
+            this.currOpenedDropdownIdx = idx
+        },
+        // closeDropdown() {
+        //     this.navBtns[currOpenedDropdownIdx].isOpen = false
+        //     this.currOpenedDropdown = null
+        // },
+    },
+    components: {
+        navDropdown,
+    },
 }
-
-
 </script>
 
 <style>
 .modal {
-
-  background-color: rgb(130, 130, 130);
-  width: 300px;
-  border-radius: 3px;
-  height: 50px;
-  position: absolute;
-  top: 44px;
+    background-color: rgb(130, 130, 130);
+    width: 300px;
+    border-radius: 3px;
+    height: 50px;
+    position: absolute;
+    top: 44px;
 }
 </style>
---
+++ frontend/src/cmps/nav-dropdown.vue
@@ -0,0 +1,55 @@
+<template>
+    <section class="nav-dropdown">
+        <header class="flex align-center justify-center">
+            <h4 v-if="btn">{{ btn.name }}</h4>
+            <button @click="toggleDropdown">x</button>
+        </header>
+        <main>
+            <article v-if="btn.name === 'Boards'" class="boards-nav">
+                <div class="curr-board" v-for="board in boards">
+                    <h4>{{board.name}}</h4>
+                    <div class="board-link flex">
+                        <div class="icon flex align-center justify-center">F</div>
+                        <h4>Frello Board</h4>
+                    </div>
+                </div>
+            </article>
+            <article v-if="btn.name === 'Recent'" class="recent-nav">
+                <div class="recent-board" v-for="board in boards">
+                    <h4>{{board.name}}</h4>
+                    <div class="board-link flex">
+                        <div class="icon flex align-center justify-center">F</div>
+                        <h4>Frello Board</h4>
+                    </div>
+                </div>
+            </article>
+        </main>
+        <footer>
+            <slot name="footer"></slot>
+        </footer>
+    </section>
+</template>
+<script>
+export default {
+    name: 'nav-dropdown',
+    props: { btn: Object },
+    data() {
+        return {
+            boards: [{name: 'Current Board'}, {name: 'Your Boards'}]
+        }
+    },
+    created() {
+        console.log(this.btn);
+    },
+    methods: {
+        toggleDropdown() {
+            // this.$emit('toggleDropdown', this.btn.name)
+        }
+    },
+    computed: {
+        // this.$store.getters.
+    },
+    unmounted() {},
+    components: {},
+}
+</script>
