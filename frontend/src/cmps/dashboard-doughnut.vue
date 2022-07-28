6fa7ac6511938f219d910ed1290c95fc8d092a85
Yuval Rubin <yuvalr96@gmail.com>
2022-07-29 01:15:13 +0300

    dashboard

--- frontend/src/cmps/dashboard-doughnut.vue
+++ frontend/src/cmps/dashboard-doughnut.vue
@@ -1,29 +1,78 @@
 <template>
-  <DoughnutChart class="doughnut-chart" :chartData="testData" />
+  <DoughnutChart class="doughnut-chart" :chartData="labelData" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import { DoughnutChart } from 'vue-chart-3';
-import { Chart, registerables } from "chart.js";
+import { defineComponent } from 'vue'
+import { DoughnutChart } from 'vue-chart-3'
+import { Chart, registerables } from 'chart.js'
 
-Chart.register(...registerables);
+Chart.register(...registerables)
 
 export default defineComponent({
   name: 'doghnut-chart',
   components: { DoughnutChart },
   setup() {
-    const testData = {
+    const labelData = {
       labels: ['Paris', 'Nîmes', 'Toulon', 'Perpignan', 'Autre'],
       datasets: [
         {
-          data: [30, 40, 60, 70, 5],
-          backgroundColor: ['#77CEFF', '#0079AF', '#123E6B', '#97B0C4', '#A5C8ED'],
+          data: [],
+          backgroundColor: [],
         },
       ],
-    };
+    }
 
-    return { testData };
+    return { labelData }
   },
-});
+  created() {
+    this.labelData.labels = this.board.labels.map(l => l.title)
+    this.labelData.datasets[0].backgroundColor = this.board.labels.map(
+      l => l.color
+    )
+    this.labelData.datasets[0].data = this.fillData()
+  },
+  methods: {
+    fillData() {
+      const labelMap = {}
+      if(!this.board.groups)return
+      this.board.groups.forEach(g => {
+        if(!g.tasks) return
+        g.tasks.forEach(t => {
+            if(!t.labelIds) return
+          t.labelIds.forEach(id => {
+            if (!labelMap[id]) {
+              labelMap[id] = 0
+            }
+            labelMap[id]++
+          })
+        })
+      })
+    
+    //   const data = []
+    //   for (let label in labelMap) {
+    //     data.push(labelMap[label])
+    //   }
+    },
+
+    //     const dataMap = {}
+    //     toys.forEach(toy => {
+    //         toy.labels.forEach(label => {
+    //             if (!dataMap[label]) {
+    //                 dataMap[label] = {}
+    //                 dataMap[label].count = 0
+    //                 dataMap[label].sum = 0
+    //             }
+    //             dataMap[label].count++
+    //             dataMap[label].sum += toy.price
+    //         })
+    //     })
+    //     return dataMap;
+  },
+  computed: {
+    board() {
+      return this.$store.getters.board
+    },
+  },
+})
 </script>
